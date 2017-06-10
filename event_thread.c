24bc7667f44d157700f55652b9e3acd4b4e40114
dar <darek.stojaczyk@gmail.com>
2017-06-11 00:18:23 +0200

    Get rid of leading underscores from event_thread static function names

--- event_thread.c
+++ event_thread.c
@@ -7,14 +7,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <signal.h>
 #include "event_thread.h"
 #include "concurrent_queue.h"
-#include "log.h"
 
 #define MAX_EVENT_THREADS 32
 
@@ -35,7 +32,7 @@ static atomic_size_t g_thread_cnt;
 static struct event_thread g_threads[MAX_EVENT_THREADS];
 
 static struct event_thread *
-_get_event_thread(size_t thread_id)
+get_event_thread(size_t thread_id)
 {
     struct event_thread *thread;
 
@@ -48,9 +45,9 @@ _get_event_thread(size_t thread_id)
 }
 
 int
-enqueue_event(size_t thread_id, void (*callback)(void *, void *), void *arg1, void *arg2)
+event_thread_enqueue_event(size_t thread_id, void (*callback)(void *, void *), void *arg1, void *arg2)
 {
-    struct event_thread *thread = _get_event_thread(thread_id);
+    struct event_thread *thread = get_event_thread(thread_id);
     struct event *event;
     
     if (!thread) {
@@ -73,7 +70,7 @@ enqueue_event(size_t thread_id, void (*callback)(void *, void *), void *arg1, vo
 }
 
 static void
-_event_thread_destroy(struct event_thread *thread) {
+event_thread_destroy(struct event_thread *thread) {
     struct event *event;
 
     while (con_queue_pop(thread->events, (void **) &event) == 0) {
@@ -85,19 +82,19 @@ _event_thread_destroy(struct event_thread *thread) {
 }
 
 static void
-_sig_handler(int signo __attribute__((unused)))
+sig_handler(int signo __attribute__((unused)))
 {
     /** Interrupt any blocking IO / poll */
 }
 
 static void *
-_event_thread_loop(void *arg)
+event_thread_loop(void *arg)
 {
     struct event_thread *thread = arg;
     struct event *update_ev = NULL;
     struct event *event;
 
-    if (signal(SIGUSR1, _sig_handler) == SIG_ERR) {
+    if (signal(SIGUSR1, sig_handler) == SIG_ERR) {
         fprintf(stderr, "%s: Failed to bind SIGUSR1 handler.\n", thread->name);
         goto out;
     }
@@ -120,7 +117,7 @@ _event_thread_loop(void *arg)
 
 out:
     free(update_ev);
-    _event_thread_destroy(thread);    
+    event_thread_destroy(thread);    
     pthread_exit(NULL);
     return NULL;
 }
@@ -150,7 +147,7 @@ event_thread_create(const char *name, void (*update_cb)(void *, void *), void *a
     thread->events->size = 32;
 
     event_thread_enqueue_event(thread_id, update_cb, arg1, arg2);
-    if (pthread_create(&thread->tid, NULL, _event_thread_loop, thread) != 0) {
+    if (pthread_create(&thread->tid, NULL, event_thread_loop, thread) != 0) {
         fprintf(stderr, "Fatal: pthread_create() failed, cannot start event thread.\n");
         goto events_err;
     }
@@ -166,7 +163,7 @@ err:
 }
 
 static void
-_event_thread_stop_cb(void *arg1, void *arg2 __attribute__((unused)))
+event_thread_stop_cb(void *arg1, void *arg2 __attribute__((unused)))
 {
     struct event_thread *thread = arg1;
     
@@ -178,13 +175,13 @@ event_thread_stop(size_t thread_id)
 {
     struct event_thread *thread;
 
-    thread = _get_event_thread(thread_id);
+    thread = get_event_thread(thread_id);
     if (!thread) {
         fprintf(stderr, "Trying to stop inexistent event thread %zu.\n", thread_id);
         return -1;
     }
     
-    if (event_thread_enqueue_event(thread_id, _event_thread_stop_cb, thread, NULL) != 0) {
+    if (event_thread_enqueue_event(thread_id, event_thread_stop_cb, thread, NULL) != 0) {
         fprintf(stderr, "Failed to stop thread %zu.\n", thread_id);
         return -1;
     }
