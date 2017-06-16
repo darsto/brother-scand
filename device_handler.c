58ee14ddc7964b67f12413f4e3433189a7c5033a
dar <darek.stojaczyk@gmail.com>
2017-06-16 20:18:35 +0200

    Added very primitive device status check in device_handler

--- device_handler.c
+++ device_handler.c
@@ -10,77 +10,62 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include "device_handler.h"
-#include "log.h"
 #include "event_thread.h"
 #include "iputils.h"
-
-struct scanner_data_t {
-    char my_ip[16]; //just IPv4 for now
-    char dest_ip[16];
-    char name[16];
-    char options_num;
-    struct scanner_data_option_t {
-        char func[8];
-        char appnum;
-    } options[8];
-};
+#include "ber/snmp.h"
+#include "log.h"
 
 static int g_sockfd;
-static struct scanner_data_t g_scanner_data = {
-    .dest_ip = "10.0.0.149",
-    .name = "open-source-bro",
-    .options_num = 3,
-    .options = {
-        {.func = "IMAGE", .appnum = 1},
-        {.func = "EMAIL", .appnum = 2},
-        {.func = "FILE", .appnum = 5}
-    }
-};
+static char g_local_ip[16];
+static uint8_t g_buf[1024];
+static uint8_t *const g_buf_end = g_buf + sizeof(g_buf) - 1;
+
+static uint32_t brInfoPrinterUStatusOID[] = { 1, 3, 6, 1, 4, 1, 2435, 2, 3, 9, 4, 2, 1, 5, 5, 6, 0, SNMP_MSG_OID_END };
 
 static void
-device_handler_dev_register_cb(void *arg1, void *arg2)
+device_handler_loop(void *arg1, void *arg2)
 {
-    unsigned char buf[2048];
-    ssize_t sent_len;
+    uint8_t *out;
+
+    struct snmp_msg_header msg_header = {};
+    struct snmp_varbind varbind = {};
+
+    struct sockaddr_in sin_serv;
     ssize_t recv_len;
-    socklen_t slen = sizeof(struct sockaddr_in);
-    struct sockaddr_in server;
-    struct scanner_data_t *scanner_data = &g_scanner_data;
+    socklen_t slen = sizeof(sin_serv);
+    
+    msg_header.snmp_ver = 0;
+    msg_header.community = "public";
+    msg_header.pdu_type = SNMP_DATA_T_PDU_GET_REQUEST;
+    msg_header.request_id = 314;
 
-    server.sin_addr.s_addr = inet_addr(scanner_data->dest_ip);
-    server.sin_family = AF_INET;
-    server.sin_port = htons(161);
+    varbind.value_type = SNMP_DATA_T_NULL;
+    varbind.oid = brInfoPrinterUStatusOID;
 
-    recv_len = 0; //TODO
+    out = snmp_encode_msg(g_buf_end, &msg_header, 1, &varbind);
+    recv_len = g_buf_end - out + 1;
 
-    if (recv_len <= 0) {
-        fprintf(stderr, "Could not construct welcome message.\n");
-        return;
-    }
-
-    printf("Sending msg to %s:\n", scanner_data->dest_ip);
-    hexdump("payload", buf, (size_t) recv_len);
-    sent_len = sendto(g_sockfd, buf, recv_len, 0, (struct sockaddr *) &server, slen);
-    if (sent_len < 0) {
+    sin_serv.sin_addr.s_addr = inet_addr("10.0.0.149");
+    sin_serv.sin_family = AF_INET;
+    sin_serv.sin_port = htons(161);
+    
+    hexdump("sending", out, recv_len);
+    if (sendto(g_sockfd, out, recv_len, 0, (struct sockaddr *) &sin_serv, slen) == -1) {
         perror("sendto");
-        return;
+        goto out;
     }
+    printf("...sent!\n");
 
-    printf("Message sent. (%zd/%zd). Waiting for the reply...\n", recv_len, sent_len);
-
-    recv_len = recvfrom(g_sockfd, buf, 2048, 0, (struct sockaddr *) &server, &slen);
+    recv_len = recvfrom(g_sockfd, out, 2048, 0, (struct sockaddr *) &sin_serv, &slen);
     if (recv_len < 0) {
         perror("recvfrom");
-        return;
+        goto out;
     }
-
-    printf("Received reply from %s:\n", scanner_data->dest_ip);
-    hexdump("payload", buf, (size_t) recv_len);
-}
-
-static void
-device_handler_update_cb(void *arg1, void *arg2)
-{
+    printf("Received packet from %s:%d\n", inet_ntoa(sin_serv.sin_addr), ntohs(sin_serv.sin_port));
+    hexdump("received", out, recv_len);
+    
+out:
+    sleep(5);
 }
 
 void 
@@ -88,6 +73,11 @@ device_handler_init()
 {
     struct sockaddr_in sin;
     
+    if (iputils_get_local_ip(g_local_ip) != 0) {
+        fprintf(stderr, "Could not get local ip address.\n");
+        return;
+    }
+
     g_sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (g_sockfd == -1) {
         perror("socket");
@@ -104,9 +94,7 @@ device_handler_init()
         goto socket_err;
     }
     
-    iputils_get_local_ip(g_scanner_data.my_ip);
-    
-    event_thread_create("device_handler", device_handler_update_cb, NULL, NULL);
+    event_thread_create("device_handler", device_handler_loop, NULL, NULL);
     return;
     
 socket_err:
