8e0a6b6704e329094d72c65162421e0999172561
dar <darek.stojaczyk@gmail.com>
2017-06-18 19:56:08 +0200

    Added register_scanner_host()
    Also updated device_handler to make use of new SNMP API

--- device_handler.c
+++ device_handler.c
@@ -20,43 +20,108 @@ static char g_local_ip[16];
 static uint8_t g_buf[1024];
 static uint8_t *const g_buf_end = g_buf + sizeof(g_buf) - 1;
 
-static uint32_t brInfoPrinterUStatusOID[] = { 1, 3, 6, 1, 4, 1, 2435, 2, 3, 9, 4, 2, 1, 5, 5, 6, 0, SNMP_MSG_OID_END };
+static int
+get_scanner_status(int conn)
+{
+    struct snmp_msg_header msg_header = {};
+    struct snmp_varbind varbind = {};
+    int msg_len;
+    uint8_t *out;
+    int rc = -1;
+
+    static uint32_t brInfoPrinterUStatusOID[] = { 1, 3, 6, 1, 4, 1, 2435, 2, 3, 9, 4, 2, 1, 5, 5, 6, 0, SNMP_MSG_OID_END };
+
+    msg_header.snmp_ver = 0;
+    msg_header.community = "public";
+    msg_header.pdu_type = SNMP_DATA_T_PDU_GET_REQUEST;
+    msg_header.request_id = 0;
+
+    varbind.oid = brInfoPrinterUStatusOID;
+    varbind.value_type = SNMP_DATA_T_NULL;
+
+    out = snmp_encode_msg(g_buf_end, &msg_header, 1, &varbind);
+    msg_len = (int) (g_buf_end - out + 1);
+
+    hexdump("sending", out, msg_len);
+    msg_len = network_udp_send(conn, out, msg_len) != 0;
+    if (msg_len < 0) {
+        perror("sendto");
+        goto out;
+    }
+    
+    msg_len = network_udp_receive(conn, out, 1024);
+    if (msg_len < 0) {
+        perror("recvfrom");
+        goto out;
+    }
+    hexdump("received", out, msg_len);
+    
+    rc = 0;
+out:
+    return rc;
+}
+
+static int
+register_scanner_host(int conn)
+{
+    struct snmp_msg_header msg_header = {};
+    struct snmp_varbind varbind[3] = {};
+    int msg_len;
+    uint8_t *out;
+    char msg[3][768];
+    int i, rc = -1;
+    
+    uint32_t brRegisterKeyInfoOID[] = { 1, 3, 6, 1, 4, 1, 2435, 2, 3, 9, 2, 11, 1, 1, 0, SNMP_MSG_OID_END };
+    const char *host_name = "darsto-br1";
+    const char *scan_func[3] = { "IMAGE", "SCAN", "FILE" };
+    
+    msg_header.snmp_ver = 0;
+    msg_header.community = "internal";
+    msg_header.pdu_type = SNMP_DATA_T_PDU_SET_REQUEST;
+    msg_header.request_id = 0;
+
+    for (i = 0; i < 3; ++i) {
+        snprintf(msg[i], sizeof(msg[i]),
+                 "TYPE=BR;BUTTON=SCAN;USER=\"%s\";FUNC=%s;HOST=%s:%d;APPNUM=1;DURATION=360;CC=1;",
+                 host_name, scan_func[i], g_local_ip, 54925);
+        
+        varbind[i].oid = brRegisterKeyInfoOID;
+        varbind[i].value_type = SNMP_DATA_T_OCTET_STRING;
+        varbind[i].value.s = msg[i];
+    }
+
+    out = snmp_encode_msg(g_buf_end, &msg_header, 3, varbind);
+    msg_len = (int) (g_buf_end - out + 1);
+
+    hexdump("sending", out, msg_len);
+    msg_len = network_udp_send(conn, out, msg_len) != 0;
+    if (msg_len < 0) {
+        perror("sendto");
+        goto out;
+    }
+
+    msg_len = network_udp_receive(conn, out, 1024);
+    if (msg_len < 0) {
+        perror("recvfrom");
+        goto out;
+    }
+    hexdump("received", out, msg_len);
+
+    rc = 0;
+out:
+    return rc;
+}
 
 static void
 device_handler_loop(void *arg1, void *arg2)
 {
     int *conn = arg1;
-    struct snmp_msg_header msg_header = {};
-    struct snmp_varbind varbind = {};
-    int msg_len;
-    uint8_t *out;
+    int status;
 
-    msg_header.snmp_ver = 0;
-    msg_header.community = "public";
-    msg_header.pdu_type = SNMP_DATA_T_PDU_GET_REQUEST;
-    msg_header.request_id = 314;
-
-    varbind.value_type = SNMP_DATA_T_NULL;
-    varbind.oid = brInfoPrinterUStatusOID;
-
-    out = snmp_encode_msg(g_buf_end, &msg_header, 1, &varbind);
-    msg_len = (int) (g_buf_end - out + 1);
-    
-    hexdump("sending", out, msg_len);
-    msg_len = network_udp_send(*conn, out, msg_len) != 0;
-    if (msg_len < 0) {
-        perror("sendto");
-        goto out;
+    status = get_scanner_status(*conn);
+    if (status == 0) {
+        register_scanner_host(*conn);
     }
-    printf("...sent!\n");
-
-    msg_len = network_udp_receive(*conn, out, 1024);
-    if (msg_len < 0) {
-        perror("recvfrom");
-        goto out;
-    }
-    printf("...received!\n");
-    hexdump("received", out, msg_len);
     
 out:
     sleep(5);
